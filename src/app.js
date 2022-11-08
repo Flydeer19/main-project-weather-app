0b5e99aefbdc6c8d86657932af1ea37f064c4a4c
Flydeer19 <114246352+Flydeer19@users.noreply.github.com>
2022-11-07 18:18:56 -0700

    started the new js file from scratch
    replaced the weather icon to png instead of fontawesome icons;
    added the current temperature, city name and the weather description to js

--- src/app.js
+++ src/app.js
@@ -1,110 +1,15 @@
-function showDate (date) {
-    let days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-    ];
-    let currentDay = days[date.getDay()];
-    let currentHour = date.getHours();
-        if (currentHour < 10) {
-            currentHour = `0${currentHour}`;
-        }
-    let currentMinute = date.getMinutes();
-        if (currentMinute < 10) {
-        currentMinute = `0${currentMinute}`;
-        }
-    let dateResult = `${currentDay} ${currentHour}:${currentMinute}`;
-    return dateResult;
-}
-
-let currentDate = document.querySelector("#current-day");
-currentDate.innerHTML = showDate((new Date()));
-
-
-
-
-function showCity(event) {
-    event.preventDefault();
-    let displayedCity = document.querySelector("#city");
-    let inputCity = document.querySelector("#input-field").value;
-    inputCity = inputCity.toLowerCase();
-    inputCity = inputCity.trim();
-    let wordPart = inputCity.split(" ");
-    for (let i=0; i < wordPart.length; i++) {
-        wordPart[i] = wordPart[i].charAt(0).toUpperCase() + wordPart[i].slice(1);
-    }
-    displayedCity.innerHTML = `${wordPart.join(" ")}`;
-}
-
-let searchBar = document.querySelector("#search-bar");
-searchBar.addEventListener("submit", showCity);
-searchBar.addEventListener("submit", showTemperatureCelcias);
-
-function showCurrentTemp(response) {
-    let currentTemp = Math.round(response.data.main.temp);
-    let currentTempCelcias = document.querySelector("#current-temperature");
-    currentTempCelcias.innerHTML = currentTemp;
-}
-
-function showTemperatureCelcias (event) {
-    event.preventDefault();
-
-    let inputCity = document.querySelector("#input-field").value;
-    inputCity = inputCity.toLowerCase();
-    inputCity = inputCity.trim();
-    let wordPart = inputCity.split(" ");
-    for (let i=0; i < wordPart.length; i++) {
-        wordPart[i] = wordPart[i].charAt(0).toUpperCase() + wordPart[i].slice(1);
-    }
-
-    let apiKey = "8ca7dd4e61360b90fb66918853670e48";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${wordPart.join(" ")}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(showCurrentTemp);
-
-}
-
-function retrieveCoords (position) {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    let apiKey = "8ca7dd4e61360b90fb66918853670e48";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(showCurrentTemp);
-}
-
-function showLocation() {
-    navigator.geolocation.getCurrentPosition(retrieveCoords);
-}
-
-let currentLocation = document.querySelector("#current-location");
-currentLocation.addEventListener("click", showLocation);
-currentLocation.addEventListener("click", showGeolocation);
-
-function retrieveCityName(response) {
+function displayTemperature(response) {
     console.log(response);
-    let currentCityName = response.data.name;
-    let currentCountryCode = response.data.sys.country;
-    let displayedCity = document.querySelector("#city");
-    displayedCity.innerHTML = `${currentCityName}, ${currentCountryCode}`;
+    let temperatureElement = document.querySelector("#current-temperature");
+    let cityElement = document.querySelector("#city");
+    let conditionElement = document.querySelector("#condition");
+
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    conditionElement.innerHTML = response.data.weather[0].description;
 }
 
+let apiKey = "8ca7dd4e61360b90fb66918853670e48";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
-function showCurrentCity(position) {
-
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    let apiKey = "8ca7dd4e61360b90fb66918853670e48";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(retrieveCityName)
-
-}
-
-
-function showGeolocation() {
-navigator.geolocation.getCurrentPosition(showCurrentCity);
-}
+axios.get(apiUrl).then(displayTemperature);
