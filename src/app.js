d91a9b59882450d912c729bb5f7ee4e8327cac90
Flydeer19 <114246352+Flydeer19@users.noreply.github.com>
2022-11-08 10:53:16 -0700

    Added the humidity and windSpeed elements

--- src/app.js
+++ src/app.js
@@ -3,10 +3,14 @@ function displayTemperature(response) {
     let temperatureElement = document.querySelector("#current-temperature");
     let cityElement = document.querySelector("#city");
     let conditionElement = document.querySelector("#condition");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     conditionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "8ca7dd4e61360b90fb66918853670e48";
