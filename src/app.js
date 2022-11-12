0988e6f9ccdce6b3ee9928ae9df4cd9d3f6a68a3
Flydeer19 <114246352+Flydeer19@users.noreply.github.com>
2022-11-11 17:01:47 -0700

    added api for the forecast

--- src/app.js
+++ src/app.js
@@ -22,7 +22,9 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+    console.log(response.data);
+
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = `<div class="row">`;
@@ -44,6 +46,12 @@ function displayForecast() {
     forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+    let apiKey = "8ca7dd4e61360b90fb66918853670e48";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+ 
+    axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#current-temperature");
@@ -62,6 +70,8 @@ function displayTemperature(response) {
     windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+    getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -102,8 +112,6 @@ function showCelciusTemperature(event) {
 }
 
 let celciusTemperature = null;
-
-displayForecast();
 
 let form = document.querySelector("#search-bar");
 form.addEventListener("submit", handleSubmit);
